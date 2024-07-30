6db24abcd6ee4eec8438a79f0047a6478b043424
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-07-30 22:33:18 +0500

    footerni qilishim kerak

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './components/header/header';
+import Hero from './components/hero/hero';
+import Sevvice from './components/sevvice/sevvice';
+import Mextreo from './components/maxtreo/maxtreo';
+import Years from './components/years/years';
+import Testimonial from './components/testimonial/testimonial';
+import Letest from './components/letest/letesgt';
+import Forget from './components/forget/forget';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Hero/>
+      <Sevvice/>
+      <Mextreo/>
+      <Years/>
+      <Testimonial/>
+      <Letest/>
+      <Forget/>
     </div>
   );
 }
